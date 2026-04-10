973a53915b102742fcdd14a1c8aee08337069860
Nagendra babu <Nagendrababu.M@dynamicseshop.com>
2026-04-10 15:48:01 +0530

    Add project files.

---
+++ CURDMVC/Controllers/UserController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+
+namespace CURDMVC.Controllers
+{
+    public class UserController : Controller
+    {
+
+        #region Login / Logout
+
+            public async Task<IActionResult> Login()
+            {
+                await Task.Delay(1);
+                return  View();
+            }
+
+
+            [HttpPost]
+            public async Task<IActionResult> Login(string username, string password)
+            {
+                // Example validation (replace with DB check)
+                if (username == "admin" && password == "123")
+                {
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, username),
+                        new Claim(ClaimTypes.Role, "Admin")
+                    };
+
+                    var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    var principal = new ClaimsPrincipal(identity);
+
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+                    //(Set Expiry for 30 mins)
+                    //await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,principal,
+                    //    new AuthenticationProperties
+                    //    {
+                    //        IsPersistent = true,
+                    //        ExpiresUtc = DateTime.UtcNow.AddMinutes(30)
+                    //    });
+
+                return RedirectToAction("Index", "Home");
+                }
+
+                ViewBag.Error = "Invalid login";
+                return View();
+            }
+
+
+            public async Task<IActionResult> Logout()
+            {
+                await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+                return RedirectToAction("Login");
+            }
+
+
+        #endregion
+
+        [Authorize]
+            public IActionResult Dashboard()
+            {
+                return View();
+            }
+
+
+
+
+
+
+    }
+}
---
+++ CURDMVC/RouteServices/RouteConfig.cs
@@ -0,0 +1,27 @@
+﻿namespace CURDMVC.RouteServices
+{
+    public static class RouteConfig
+    {
+        public static void RegisterRoutes(IEndpointRouteBuilder endpoints)
+        {
+            // Default route
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+            // Login 
+            endpoints.MapControllerRoute(
+                name: "Login",
+                pattern: "User/Login",
+                defaults: new { controller = "User", action = "Login" });
+
+
+            // Custom route example
+            endpoints.MapControllerRoute(
+                name: "productroute",
+                pattern: "items/{id}",
+                defaults: new { controller = "Product", action = "Details" });
+        }
+    }
+}
